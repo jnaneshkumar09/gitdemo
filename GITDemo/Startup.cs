53cc2d4aa9804b78f8528ea7bd4113c5e6eccf83
jnanesh kumar <jnanesh@digixhub.com>
2018-10-02 17:13:47 +0530

    Add project files.

---
+++ GITDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GITDemo.Startup))]
+namespace GITDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
